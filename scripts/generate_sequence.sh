9497baf8e2dafdd7d18620b18349e0f76325a474
Samod Shetty <samod.shetty@gmail.com>
2021-11-16 17:12:48 -0800

    Updated generate_sequence to be able to run from home folder

--- scripts/generate_sequence.sh
+++ scripts/generate_sequence.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-find ../data/winwebsec/ -iname "log_api.txt"  -exec python seq-gen.py {} \; &> ../data/sequences/winwebsec_ver2.txt
-find ../data/harebot/ -iname "log_api.txt"  -exec python seq-gen.py {} \; &> ../data/sequences/harebot_ver2.txt
-find ../data/smarthdd/ -iname "log_api.txt"  -exec python seq-gen.py {} \; &> ../data/sequences/smarthdd_ver2.txt
-find ../data/cridex/ -iname "log_api.txt"  -exec python seq-gen.py {} \; &> ../data/sequences/cridex_ver2.txt
-find ../data/Injector_Trojan/ -iname "log_api.txt"  -exec python seq-gen.py {} \; &> ../data/sequences/Injector_Trojan_ver2.txt
-find ../data/zbot/ -iname "log_api.txt"  -exec python seq-gen.py {} \; &> ../data/sequences/zbot_ver2.txt
-find ../data/zeroaccess/ -iname "log_api.txt"  -exec python seq-gen.py {} \; &> ../data/sequences/zeroaccess_ver2.txt
-find ../data/securityshield/ -iname "log_api.txt"  -exec python seq-gen.py {} \; &> ../data/sequences/securityshield_ver2.txt
-find ../data/trojan/ -exec python seq-gen.py {} \; &> ../data/sequences/trojan_ver2.txt
-find ../data/obfuscationLogs/ -iname "log_api.txt" -exec python seq-gen.py {} \; &> ../data/sequences/enigma_protect.txt
-find ../data/appLogs/ -exec python seq-gen.py {} \; &> ../data/sequences/app_trunc100.txt
-find ../data/ransom/ -iname "log_api.txt"  -exec python seq-gen.py {} \; &> ../data/sequences/ransom_ver2.txt
+find data/winwebsec/ -iname "log_api.txt"  -exec python scripts/seq-gen.py {} \; &> data/sequences/winwebsec_ver2.txt
+find data/harebot/ -iname "log_api.txt"  -exec python scripts/seq-gen.py {} \; &> data/sequences/harebot_ver2.txt
+find data/smarthdd/ -iname "log_api.txt"  -exec python scripts/seq-gen.py {} \; &> data/sequences/smarthdd_ver2.txt
+find data/cridex/ -iname "log_api.txt"  -exec python scripts/seq-gen.py {} \; &> data/sequences/cridex_ver2.txt
+find data/Injector_Trojan/ -iname "log_api.txt"  -exec python scripts/seq-gen.py {} \; &> data/sequences/Injector_Trojan_ver2.txt
+find data/zbot/ -iname "log_api.txt"  -exec python scripts/seq-gen.py {} \; &> data/sequences/zbot_ver2.txt
+find data/zeroaccess/ -iname "log_api.txt"  -exec python scripts/seq-gen.py {} \; &> data/sequences/zeroaccess_ver2.txt
+find data/securityshield/ -iname "log_api.txt"  -exec python scripts/seq-gen.py {} \; &> data/sequences/securityshield_ver2.txt
+find data/trojan/ -exec python scripts/seq-gen.py {} \; &> data/sequences/trojan_ver2.txt
+find data/obfuscationLogs/ -iname "log_api.txt" -exec python scripts/seq-gen.py {} \; &> data/sequences/enigma_protect.txt
+find data/appLogs/ -exec python scripts/seq-gen.py {} \; &> data/sequences/app_trunc100.txt
+find data/ransom/ -iname "log_api.txt"  -exec python scripts/seq-gen.py {} \; &> data/sequences/ransom_ver2.txt
